8917aa9b04cdec4eccae195e440847ac1df7ab62
liviya-cmd <liviyaks143@gmail.com>
2024-12-16 15:45:33 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Container(
         child: Center(
-          child: Text('welcome mister kumar'),
+          child: Text('welcome  kumar'),
         ),
       ),
     );
